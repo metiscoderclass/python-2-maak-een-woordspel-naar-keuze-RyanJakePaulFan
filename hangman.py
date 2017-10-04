a046f02aa38bcdd85e1533da06f3aa0b97087dce
Ryan <ryan.lamsberg@coderclass.nl>
2017-10-04 23:22:11 +0200

    Inputs

---
+++ hangman.py
@@ -0,0 +1,18 @@
+naam = input("Hallo wat is je naam?")
+print("Hallo" + naam, "welkom bij galgje!")
+
+woord = input("Geef een woord op")
+
+
+while True:
+
+    woordraden = input("Wil je het woord raden met ?")
+    letter = input("Geef een letter op")
+
+    if letter goed:
+
+
+
+
+
+
