5ac4ec84dcef9b4069cae75f2ebab40baf5a5749
Ryan <ryan.lamsberg@coderclass.nl>
2017-10-08 23:21:53 +0200

    Lijst toegevoegd, nogsteeds errors

--- hangman.py
+++ hangman.py
@@ -1,15 +1,25 @@
 naam = input("Hallo wat is je naam?")
 print("Hallo" + naam, "welkom bij galgje!")
 
-woord = input("Geef een woord op")
+list = [' banaan', 'soep', 'woord', 'coderclass', 'issam', 'codercrackers', 'huis', 'boom', 'midas', 'baggerbusters', 'python', 'html', 'css', 'javascript']
+woorden = random.choice(list)
 
+countfouten = + 1
+geraden = + 1
+rondes = 5
 
 while True:
+    letter = input("Geef een letter op of raad het woord raden met ? ")
+    if letter in woorden:
+        print("Goedzo")
+    elif vraagteken:
+        raad = input("Raad het woord")
+    if ranad in woorden:
+        print("Goedzo je hebt het goed!")
 
-    woordraden = input("Wil je het woord raden met ?")
-    letter = input("Geef een letter op")
 
-    if letter goed:
+
+
 
 
 
