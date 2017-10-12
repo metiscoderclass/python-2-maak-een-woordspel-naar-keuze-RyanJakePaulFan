a33eb4929a701f5b7ee9a513ecc158dec2e04e09
Ryan <ryan.lamsberg@coderclass.nl>
2017-10-12 09:27:56 +0200

    Defs

--- hangman.py
+++ hangman.py
@@ -1,21 +1,42 @@
-naam = input("Hallo wat is je naam?")
-print("Hallo" + naam, "welkom bij galgje!")
+import random
+
+naam = input("Hallo wat is je naam? ")
+print("Hallo " + naam, "welkom bij galgje!")
 
 list = [' banaan', 'soep', 'woord', 'coderclass', 'issam', 'codercrackers', 'huis', 'boom', 'midas', 'baggerbusters', 'python', 'html', 'css', 'javascript']
-woorden = random.choice(list)
+woord = random.choice(list)
+print (woord)
 
 countfouten = + 1
 geraden = + 1
 rondes = 5
+goedeletter = ''
+
+def galg1(galg1):
+    print(--------)
+print(galg1(galg1)
+
+
+
+
+
 
 while True:
     letter = input("Geef een letter op of raad het woord raden met ? ")
-    if letter in woorden:
-        print("Goedzo")
-    elif vraagteken:
-        raad = input("Raad het woord")
-    if ranad in woorden:
-        print("Goedzo je hebt het goed!")
+    raad = input("Raad het woord")
+
+    if letter in woord:
+        print("Goedzo dat was de goede letter")
+        goedeletter = (goedeletter + letter)
+    elif letter == "?":
+        if raad in woord:
+            print("Goedzo je hebt het goed!")
+            print(geraden)
+    else:
+        print("Jammer niet de goede letter")
+        aantalfouten = + 1
+
+
 
 
 
